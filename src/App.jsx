493546c310c5c7e10aa287624d42d8b38b578f4a
lucky <luckykumaridos2002@gmail.com>
2025-09-09 23:44:21 +0530

    updated changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/sidebar.jsx'
+import Main from './components/Main/Main.jsx'
 
 const App = () => {
   return (
     <>
     <Sidebar/>
+    <Main/>
     </>
   )
 }
